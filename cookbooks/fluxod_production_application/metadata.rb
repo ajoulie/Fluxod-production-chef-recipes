2ea14ae98b647e685244c2465d2420a3dd123e0a
ajoulie <antoine.joulie@gmail.com>
2011-12-27 14:08:06 +0100

    fix metadata.rb

--- cookbooks/fluxod_production_application/metadata.rb
+++ cookbooks/fluxod_production_application/metadata.rb
@@ -5,4 +5,4 @@ description      "Installs/Configures fluxod_production_application"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
-recipe 'fluxod_production_application:helloword', "log 'hello word'"
+recipe 'fluxod_production_application::helloword', "log 'hello word'"
