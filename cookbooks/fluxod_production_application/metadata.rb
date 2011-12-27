f461526696a18d08e1e7d49590cf3770ac66e1da
ajoulie <antoine.joulie@gmail.com>
2011-12-27 11:54:06 +0100

    adding cookbook fluxod_production_application

---
+++ cookbooks/fluxod_production_application/metadata.rb
@@ -0,0 +1,8 @@
+maintainer       "YOUR_COMPANY_NAME"
+maintainer_email "YOUR_EMAIL"
+license          "All rights reserved"
+description      "Installs/Configures fluxod_production_application"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+
+recipe 'fluxod_production_application:default', "log 'hello word'"
---
+++ cookbooks/fluxod_production_application/recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: fluxod_production_application
+# Recipe:: default
+#
+# Copyright 2011, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+#
+log 'hello word'
